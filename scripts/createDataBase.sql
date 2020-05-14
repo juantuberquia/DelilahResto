17cea56df06cc5a662b8222a3a6bb06523c3c70d
juantuberquia <juantuberquia@users.noreply.github.com>
2020-05-14 18:00:38 -0500

    create folder script

---
+++ scripts/createDataBase.sql
@@ -0,0 +1,19 @@
+-- query administrador DB
+INSERT INTO `usuarios`(
+    `usuario`,
+    `nombreCompleto`,
+    `correo`,
+    `telefono`,
+    `direccion`,
+    `contraseña`,
+    `esAdmin`
+)
+VALUES(
+    "4dministr4dor",
+    "4dministr4dor",
+    "4dministr4dor",
+     4020,
+    "4dministr4dor",
+    "4dmin40",
+    TRUE
+)
