d28fb10af8c8e7eb99a390c2814d974ccf631d5b
juantuberquia <juantuberquia@users.noreply.github.com>
2020-05-18 16:11:41 -0500

    operacion crud la tabla productos con validacion de admin

--- models/producto.js
+++ models/producto.js
@@ -7,11 +7,48 @@ const productos = {};
 
 productos.agregarProducto = async (req) => {
   const { nombre, precio } = req.body;
-
   const resultado = await sequelize.query(
     "INSERT INTO Productos( nombre, precio) VALUES ( ?,?)",
     {
       replacements: [nombre, precio],
+    }
+  );
+  return resultado;
+};
+
+productos.obtenerPorID = async (req) => {
+  const id = req.query.id;
+
+  const resultado = await sequelize.query(
+    " SELECT  nombre, precio FROM `Productos` WHERE idProducto = ? ",
+    {
+      replacements: [id],
+      type: sequelize.QueryTypes.SELECT,
+    }
+  );
+  return resultado;
+};
+
+productos.actualizarPorID = async (req) => {
+  const nombre = req.body.nombre;
+  const id = req.query.id;
+
+  const resultado = await sequelize.query(
+    " UPDATE Productos SET nombre=?  WHERE idProducto = ?",
+    {
+      replacements: [nombre, id],
+    }
+  );
+  return resultado;
+};
+
+productos.eliminarProducto = async (req) => {
+  const id = req.query.id;
+
+  const resultado = await sequelize.query(
+    "DELETE FROM `Productos` WHERE idProducto = ?",
+    {
+      replacements: [id],
     }
   );
   return resultado;
--- routes/productos.js
+++ routes/productos.js
@@ -1,10 +1,25 @@
 const { Router } = require("express");
 const router = Router();
 const producto = require("../models/producto");
+const validaUsuario = require("../middleware/validarUsuario");
 
-router.route("/").post(async (req, res) => {
-  await producto.agregarProducto(req);
-  res.json("producto agregado correctamente");
-});
+router
+  .route("/")
+  .post(validaUsuario, async (req, res) => {
+    await producto.agregarProducto(req);
+    res.json("producto agregado correctamente");
+  })
+  .get(validaUsuario, async (req, res) => {
+    const ObtenerProducto = await producto.obtenerPorID(req);
+    res.json(ObtenerProducto);
+  })
+  .put(validaUsuario, async (req, res) => {
+    await producto.actualizarPorID(req);
+    res.json("Nombre del producto actualizado");
+  })
+  .delete(validaUsuario, async (req, res) => {
+    await producto.eliminarProducto(req);
+    res.json("producto eliminado exitosamente");
+  });
 
 module.exports = router;
